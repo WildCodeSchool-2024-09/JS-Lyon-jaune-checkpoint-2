238430c66410e6eac403312d805b57a42e1c3ad5
Faccinetto <bastien.faccinetto@gmail.com>
2024-12-02 10:52:46 +0100

    Step 2 terminé, map depuis API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import NavBar from "./components/NavBar";
 
 function App() {
-  useEffect(() => {
-    fetch("http://localhost:3310/api/cupcakes")
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        console.info(result);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3310/api/cupcakes")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((result) => {
+  //       console.info(result);
+  //     });
+  // }, []);
   return (
     <>
       <main className="main-container">
--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
@@ -38,7 +40,17 @@ const sampleCupcakes = [
 /* ************************************************************************* */
 
 function CupcakeList() {
+  const [cupcakes, setCupcakes] = useState(sampleCupcakes);
   // Step 1: get all cupcakes (with useEffect)
+  useEffect(() => {
+    fetch("http://localhost:3310/api/cupcakes")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCupcakes(data);
+      });
+  }, []);
 
   // Step 3: get all accessories
 
@@ -59,6 +71,12 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {cupcakes.map((cupcake) => (
+          <li className="cupcake-item" key={cupcake.id}>
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
+
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
           <Cupcake data={sampleCupcakes[0]} />
